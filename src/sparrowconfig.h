b119fc0d2f6ff35b9949f2c42071ac1ee346f7cf
Douglas Bagnall <douglas@paradise.net.nz>
2010-04-07 00:02:32 +1200

    bring in some pieces of autoconf conf

---
+++ src/sparrowconfig.h
@@ -0,0 +1,14 @@
+/* gst bits */
+#define GST_LICENSE "LGPL"
+#define GST_PACKAGE_NAME "GStreamer sparrow plugin"
+#define GST_PACKAGE_ORIGIN "what exactly do you mean by 'origin'?"
+
+#ifndef VERSION
+#define VERSION "1"
+#endif
+
+#ifndef PACKAGE
+#define PACKAGE "sparrow"
+#endif
+
+
