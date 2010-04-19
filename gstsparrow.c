6f972ef2d8ac77cc007465854e73eb4dc7031a6b
Douglas Bagnall <douglas@paradise.net.nz>
2010-04-20 02:17:52 +1200

    gst_pad_use_fixed_caps(GST_BASE_TRANSFORM_SRC_PAD(sparrow)) appeared twice

--- gstsparrow.c
+++ gstsparrow.c
@@ -210,7 +210,6 @@ gst_sparrow_init (GstSparrow * sparrow, GstSparrowClass * g_class)
 
   /*disallow resizing */
   gst_pad_use_fixed_caps(GST_BASE_TRANSFORM_SRC_PAD(sparrow));
-  gst_pad_use_fixed_caps(GST_BASE_TRANSFORM_SRC_PAD(sparrow));
 
   GST_DEBUG_OBJECT(sparrow, "gst_sparrow_init. RNG:%p", sparrow->dsfmt);
 }
