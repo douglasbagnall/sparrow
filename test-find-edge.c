e77766c97e2985c73807a46bd6cdc0e868db4c00
Douglas Bagnall <douglas@paradise.net.nz>
2010-05-10 23:10:45 +1200

    more familiarising with openCV, found reasonable square finder.

--- test-find-edge.c
+++ test-find-edge.c
@@ -15,21 +15,8 @@
 #define IMG_IN_NAME "images/test-image.png"
 #define IMG_OUT_NAME "images/test-image-%s.png"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#define JUMPAHEAD 5
+#define JA_THRESHOLD MAX(threshold - 2, 0)
 
 
 static inline int
@@ -79,13 +66,10 @@ floodfill_mono(IplImage *im, IplImage *mim, CvPoint start, int threshold)
   while(n_starts){
     n_nexts = 0;
     int i;
-    int c;
     for (i = 0; i < n_starts; i++){
       int x = starts[i].x;
       int y = starts[i].y;
       int c = data[y * w + x];
-#define JUMPAHEAD 10
-#define JA_THRESHOLD (threshold - 2)
       if (x > 0){
         if (! expand_one_mono(x - 1, y, c, threshold, nexts, &n_nexts, data, mdata, w, h) &&
             x > JUMPAHEAD - 1)
@@ -120,111 +104,6 @@ floodfill_mono(IplImage *im, IplImage *mim, CvPoint start, int threshold)
 
 
 
-
-
-/* if point (x,y) is close enough to colour (r,g,b) in Euclidean RGB space,
-   add it to the nexts list. otherwise, not.*/
-
-static inline int
-expand_one(int x, int y, int r, int g, int b,
-    int threshold, CvPoint *nexts, int *n_nexts, guint32 *im, guint8 *mask, int w, int h){
-  guint8 *p = (guint8*) &im[y * w + x];
-  guint8 *m = &mask[y * w + x];
-  //printf("looking at point %d, %d. rgb is %x.%x.%x, threshold %d, p %x %x %x mask %x\n",
-  //    x, y, r, g, b, threshold, p[0], p[1], p[2], *m);
-  if (*m){
-    int diff = ((p[0] - r) * (p[0] - r) +
-        (p[1] - g) * (p[1] - g) +
-        (p[2] - b) * (p[2] - b));
-    //debug("diff %d\n", diff);
-    if (diff  <= threshold){
-      *m = 0;
-      nexts[*n_nexts].x = x;
-      nexts[*n_nexts].y = y;
-      (*n_nexts)++;
-      return 1;
-    }
-    else{
-      *m = 128;
-      //if (diff <= 255) /*for obviousness calculations, later */
-      //  p[3] = diff;
-    }
-  }
-  return 0;
-}
-
-static IplImage*
-floodfill(IplImage *im, IplImage *mim, CvPoint start, int threshold)
-{
-  guint32 * data = (guint32 *)im->imageData;
-  guint8 * mdata = (guint8 *)mim->imageData;
-  int w = im->width;
-  int h = im->height;
-  int n_starts;
-  int n_nexts = 0;
-  CvPoint *starts;
-  CvPoint *nexts;
-
-  //malloc 2 lists of points. These *could* be as large as the image (but never should be)
-  starts = malloc(w * h * 2 * sizeof(CvPoint));
-  nexts = starts + w * h;
-
-  n_starts = 1;
-  starts[0] = start;
-
-  /* expand with a wavefront, each point consuming its neighbours that
-     differ from it by less than the threshold. If the neighbour is too
-     different, it tries jumping ahead in that direction, to see if there is
-     more on the other side. ("wavefront with sparks")*/
-  while(n_starts){
-    n_nexts = 0;
-    int i;
-    int r, g, b;
-    for (i = 0; i < n_starts; i++){
-      int x = starts[i].x;
-      int y = starts[i].y;
-      //printf("looking at point %d, %d\n", x, y);
-      guint8 *p = (guint8 *)&data[y * w + x];
-      r = (int)p[0];
-      g = (int)p[1];
-      b = (int)p[2];
-#define JUMPAHEAD 10
-#define JA_THRESHOLD (threshold - 2)
-      if (x > 0){
-        if (! expand_one(x - 1, y, r, g, b, threshold, nexts, &n_nexts, data, mdata, w, h) &&
-            x > JUMPAHEAD - 1)
-          expand_one(x - JUMPAHEAD, y, r, g, b, JA_THRESHOLD, nexts, &n_nexts, data, mdata, w, h);
-      }
-      if (x < w - 1){
-        if (! expand_one(x + 1, y, r, g, b, threshold, nexts, &n_nexts, data, mdata, w, h) &&
-            x < w - JUMPAHEAD - 1)
-          expand_one(x + JUMPAHEAD, y, r, g, b, JA_THRESHOLD, nexts, &n_nexts, data, mdata, w, h);
-      }
-      if (y > 0){
-        if (! expand_one(x, y - 1, r, g, b, threshold, nexts, &n_nexts, data, mdata, w, h) &&
-            y > JUMPAHEAD - 1)
-          expand_one(x, y - JUMPAHEAD, r, g, b, JA_THRESHOLD, nexts, &n_nexts, data, mdata, w, h);
-      }
-      if (y < h - 1){
-        if (! expand_one(x, y + 1, r, g, b, threshold, nexts, &n_nexts, data, mdata, w, h) &&
-            y < h - JUMPAHEAD - 1)
-          expand_one(x, y + JUMPAHEAD, r, g, b, JA_THRESHOLD, nexts, &n_nexts, data, mdata, w, h);
-      }
-    }
-    CvPoint *tmp = starts;
-    starts = nexts;
-    nexts = tmp;
-    n_starts = n_nexts;
-  }
-  free(starts < nexts ? starts : nexts);
-
-  return im;
-}
-
-
-
-
-
 static IplImage* UNUSED
 test_find_edges_gcg(IplImage *im)
 {
@@ -254,7 +133,7 @@ test_find_edges_gcg(IplImage *im)
   //return out;
 }
 
-static IplImage* UNUSED
+static UNUSED IplImage*
 test_find_edges_close_cmp(IplImage *im)
 {
   /* find the colour of the centre. it gives an approximate value to use as
@@ -274,121 +153,74 @@ test_find_edges_close_cmp(IplImage *im)
 }
 
 
-static IplImage *sharpen(IplImage *im)
-{
-  IplImage* imf = cvCreateImage(cvGetSize(im), IPL_DEPTH_32F, 1);
-  IplImage* lapl = cvCreateImage(cvGetSize(im), IPL_DEPTH_32F, 1);
-  IplImage *out = cvCreateImage(cvGetSize(im), IPL_DEPTH_8U, 1);
 
-  cvConvert(im, imf);
-
-  CvMat* kernel = cvCreateMat(3, 3, CV_32FC1);
-  cvSet(kernel, cvScalarAll(-1.0), NULL);
-  cvSet2D(kernel, 1, 1, cvScalarAll(15.0));
-
-  cvSmooth(imf, imf, CV_GAUSSIAN, 3, 0, 0, 0);
-  //cvFilter2D(imf, lapl, kernel, cvPoint(-1, -1));
-
-  cvLaplace(imf, lapl, 3);
-
-  double maxv = 0.0;
-  double minv = DBL_MAX;
-  CvPoint minl = {0, 0};
-  CvPoint maxl = {0, 0};
-
-  cvMinMaxLoc(lapl, &minv, &maxv, &minl, &maxl, NULL);
-  double range = maxv - minv;
-  double scale = 255.0 / range;
-
-  printf("max %f at %d,%d; min %f at %d,%d.\n",
-      maxv, maxl.x, maxl.y, minv, minl.x, minl.y);
-  printf("range %f. scale %f\n", range, scale);
-
-  cvConvertScale(lapl, out, scale, scale * -minv);
-
-
-  cvMinMaxLoc(im, &minv, &maxv, &minl, &maxl, NULL);
-  range = maxv - minv;
-  scale = 255.0 / range;
-
-  cvSub(im, out, im, NULL);
-
-  printf("im max %f at %d,%d; min %f at %d,%d.\n",
-      maxv, maxl.x, maxl.y, minv, minl.x, minl.y);
-  printf("range %f. scale %f\n", range, scale);
-
-
-
-  return im;
-}
-
-static IplImage* UNUSED
-test_find_edges(IplImage *im)
+static UNUSED IplImage*
+test_find_edges_hist(IplImage *im)
 {
   /* find the colour of the centre. it gives an approximate value to use as
      threshold
    */
-  int w = im->width;
-  int h = im->height;
-  CvSize size = {w, h};
-  IplImage *green = cvCreateImage(size, IPL_DEPTH_8U, 1);
-  //IplImage *mask = cvCreateImage(size, IPL_DEPTH_8U, 1);
-  //IplImage *out = cvCreateImage(size, IPL_DEPTH_8U, 1);
+  IplImage *green = cvCreateImage(cvGetSize(im), IPL_DEPTH_8U, 1);
   cvSplit(im, NULL, green, NULL, NULL);
 
-  sharpen(green);
+  IplImage *mask = cvCreateImage(cvGetSize(im), IPL_DEPTH_8U, 1);
+
+  int w = im->width;
+  int h = im->height;
+  CvSize small_size = {w / 4, h / 4};
+  IplImage *small = cvCreateImage(small_size, IPL_DEPTH_8U, 1);
+  cvResize(green, small, CV_INTER_NN);
+
+  int hist_size[] = {255};
+  float range[] = {0, 255};
+  float *ranges[] = {range};
+  CvHistogram* hist = cvCreateHist(1, hist_size, CV_HIST_ARRAY, ranges, 1);
+  cvCalcHist(&small, hist, 0, NULL);
+
+  int pixels = small->width * small->height;
+  int min_black = pixels / 8;
+  int max_black = pixels / 2;
+  int totals[256] = {0};
+
+  int best_d = pixels + 1;
+  int best_t = 0;
+
+  int total = 0;
+  for (int i = 0; i < 255; i++){
+    int v = (int)cvQueryHistValue_1D(hist, i + 2);
+    total += v;
+    totals[i] = total;
+    if (total > min_black){
+      if (i > 5){
+        int diff = totals[i] - totals[i - 5];
+        if (diff < best_d){
+          best_d = diff;
+          best_t = i;
+        }
+        if (total >= max_black){
+          break;
+        }
+      }
+    }
+  }
+  best_t -= 2;
+  printf("found best threshold %d -- %d pixel change at %d/%d pixels\n",
+      best_t, best_d, totals[best_t], pixels);
+
   //cvMorphologyEx(green, mask, NULL, NULL, CV_MOP_OPEN, 1);
-  //cvCmpS(mask, 88, mask, CV_CMP_GT);
-  //cvLaplace(green, mask, 3);
+  cvCmpS(green, best_t, mask, CV_CMP_GT);
 
-  //cvSmooth(green, mask, CV_GAUSSIAN, 3, 0, 0, 0);
-  //cvErode(mask, out, NULL, 2);
-  //cvDilate(out, mask, NULL, 2);
+  CvPoint middle = {w/2, h/2};
+  CvScalar paint = cvScalarAll(99);
+  CvScalar margin = cvScalarAll(0);
 
-  //cvSmooth(green, out, CV_GAUSSIAN, 5, 0, 0, 0);
-  //cvSub(out, mask, green, NULL);
-  //cvCanny(mask, out, 70, 190, 3);
-  //cvSmooth(out, mask, CV_GAUSSIAN, 3, 0, 0, 0);
-  //cvDistTransform(mask, out, CV_DIST_L2, 3, NULL, NULL);
-  //CvPoint middle = {w/2, h/2};
+  cvFloodFill(mask, middle, paint, margin, margin, NULL, 4, NULL);
 
-  //CvScalar paint = cvScalarAll(99);
-  //CvScalar margin = cvScalarAll(2);
-  //cvThreshold(mask, green, 50, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
-  //cvFloodFill(mask, middle, paint, margin, margin, NULL, 4, NULL);
-  return green;
-  //return mask;
-  //return out;
-}
-
-static IplImage*
-test_find_edges_floodfill(IplImage *im){
-  int w = im->width;
-  int h = im->height;
-  CvPoint centre = {w/2, h/2};
-  CvSize size = {w, h};
-  //IplImage *rgba = cvCreateImage(size, IPL_DEPTH_8U, 4);
-  //cvCvtColor(im, rgba, CV_RGB2RGBA);
-
-  IplImage *green = cvCreateImage(size, IPL_DEPTH_8U, 1);
-  cvSplit(im, NULL, green, NULL, NULL);
-
-  IplImage *mask = cvCreateImage(size, IPL_DEPTH_8U, 1);
-
-  debug("%p %p\n",&(mask->imageData), mask->imageData);
-  memset(mask->imageData, 255, w*h);
-
-  //cvSmooth(green, green, CV_GAUSSIAN, 3, 0, 0, 0);
-  //cvMorphologyEx(green, green, NULL, NULL, CV_MOP_OPEN, 1);
-
-  floodfill_mono(green, mask, centre, 0);
-
-
-  //cvSplit(im, NULL, NULL, NULL, alpha);
-  //cvSplit(im, alpha, NULL, NULL, NULL);
 
   return mask;
 }
+
+
 
 int main(int argc, char **argv)
 {
@@ -402,7 +234,8 @@ int main(int argc, char **argv)
   gettimeofday(&tv1, NULL);
 
 
-  IplImage *im_out = test_find_edges_floodfill(im_in);
+  IplImage *im_out = test_find_edges_hist(im_in);
+  //IplImage *im_out = test_find_edges(im_in);
 
 
   gettimeofday(&tv2, NULL);
