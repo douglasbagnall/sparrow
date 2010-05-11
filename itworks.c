34738e1b4bcb8980481f68fea900a3d7f0c63822
Douglas Bagnall <douglas@paradise.net.nz>
2010-05-11 17:11:14 +1200

    new file itworks.c, to do the actual stuff

---
+++ itworks.c
@@ -0,0 +1,44 @@
+/* Copyright (C) <2010> Douglas Bagnall <douglas@halo.gen.nz>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "sparrow.h"
+#include "gstsparrow.h"
+
+#include <string.h>
+#include <math.h>
+
+
+UNUSED
+static void
+simple_negation(guint8 * bytes, guint size){
+  guint i;
+  guint32 * data = (guint32 *)bytes;
+  //could use sse for superspeed
+  for (i = 0; i < size / 4; i++){
+    data[i] = ~data[i];
+  }
+}
+
+static void
+gamma_negation(GstSparrow *sparrow, guint8 *in, guint8 *out){
+  //guint i;
+  //XXX  could try oil_tablelookup_u8
+  //for (i = 0; i < size; i++){
+  //  out[i] = sparrow_rgb_gamma_full_range_REVERSE[in[i]];
+  // }
+}
