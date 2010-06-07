1fef856eeaf7174b1034cb33c4b3fa8a6c1136d5
Douglas Bagnall <douglas@paradise.net.nz>
2010-06-07 17:59:22 +1200

    loading and linking images

--- prepare_images.py
+++ prepare_images.py
@@ -5,55 +5,17 @@ import subprocess
 import array
 import struct
 import Image
-from cStringIO import StringIO
+from itertools import count
 
-from sparrow import FRAME_STRUCTURE, INDEX_FILE, TEXT_INDEX_FILE
-from sparrow import save_frames, save_frames_text
-
+from sparrow import FRAME_STRUCTURE, INDEX_FILE, TEXT_INDEX_FILE, BLOB_NAME
+from sparrow import save_frames, save_frames_text, link_frames
+from sparrow import Frame
 
 class Sequence:
     def __init__(self, seq_id):
         self.id = seq_id
         self.frames = []
         self.append = self.frames.append
-
-
-class Frame:
-    def __init__(self):
-        pass
-
-def downscale_manual(jpeg_data):
-    #XXX maybe PIL would just be quicker!
-    p = subprocess.Popen(['djpeg', '-scale', '1/8', '-greyscale'],
-                         stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-
-    out, err = p.communicate(jpeg_data)
-    # out will be scaled down by 8, ie from 800x600 to 100x75
-    # we want it down to 8x6, 4x3 or thereabouts
-    # precision doesn't matter
-    TW = 8
-    TH = 6 # targets
-    im = out.split('255\n', 1)[1]
-    im_array = array.array('B', im)
-
-    width = int((len(im) * 4 / 3) ** 0.5) #assume 4x3 (could read header!)
-    height = width * 3 / 4
-    start = height / (TH * 2) + width / (TW * 2)
-    hstep = width / TW
-    vstep = height * width / TH
-    small = []
-
-    for y in range(TH):
-        point = start + y * vstep
-        for x in range(TW):
-            total = (im_array[point - 2] +
-                     im_array[point + 2] +
-                     im_array[point + 2 * width] +
-                     im_array[point - 2 * width])
-            small.append(total >> 2)
-            point += hstep
-
-    return small
 
 
 def stretch(im, size, filter=Image.NEAREST):
@@ -76,46 +38,50 @@ def downscale_pil(jpeg_data):
     im = stretch(im, (8, 6))
     return im.tostring()
 
-def process_dir(dirname):
-    os.chdir(dirname)
-
+def process_dir(dirname, blobname=BLOB_NAME):
     fn_re = re.compile(r'(\d{3})-(\d{5}).jpg')
-    jpeg_glob = open('jpeg.glob', 'w')
-    sequences = []
+
+    jpegblob = open(blobname, 'w')
+
     frames = []
     glob_index = 0
 
-    files = sorted(x for x in os.listdir('.') if fn_re.match(x))
-    seq = Sequence(None)
-    count = 0
-    for fn in files:
-        m = fn_re.match(fn)
-        #print fn
-        new_seq_id = int(m.group(1))
-        if new_seq_id != seq.id:
-            seq = Sequence(new_seq_id)
-            sequences.append(seq)
+    files = sorted(x for x in os.listdir(dirname) if fn_re.match(x))
 
-        f = open(fn)
+    seq_id = None
+    frame_counter = count()
+    for fn in files:
+        f = open(os.path.join(dirname, fn))
         jpeg = f.read()
         f.close()
-        jpeg_glob.write(jpeg)
+        jpegblob.write(jpeg)
+
         frame = Frame()
-        frame.index = count
+        frame.index = frame_counter.next()
         frame.glob_index = glob_index
         frame.jpeg_len = len(jpeg)
         frame.summary = downscale_pil(jpeg)
-        seq.frames.append(frame)
-        frames.append(frame)
         frame.successors = [0] * 8
-        if seq.frames:
-            seq.frames[-1].successors[0] = count
 
+        m = fn_re.match(fn)
+        new_seq_id = m.group(1)
+        if new_seq_id != seq_id:
+            seq_id = new_seq_id
+        elif frames:
+            prev = frames[-1]
+            prev.successors[0] = frame.index
+
+        frames.append(frame)
         glob_index += len(jpeg)
-        count += 1
 
-    save_frames(INDEX_FILE)
-    save_frames_text(TEXT_INDEX_FILE)
+
+    save_frames(frames, INDEX_FILE + '-prelink')
+    save_frames_text(frames, TEXT_INDEX_FILE + '-prelink')
+
+    link_frames(frames)
+
+    save_frames(frames, INDEX_FILE + '-post-link')
+    save_frames_text(frames, TEXT_INDEX_FILE + '-post-link')
 
 
 
--- sparrow.py
+++ sparrow.py
@@ -1,19 +1,18 @@
 import struct
+import numpy as np
+import heapq
+from itertools import count
 
 
 INDEX_FILE = 'content/jpg/jpeg.index'
 TEXT_INDEX_FILE = 'content/jpg/jpeg.index.txt'
 FRAME_STRUCTURE = 'II48s8I'
+BLOB_NAME='jpeg.blob'
+
 
 def save_frames(frames, filename):
     f = open(filename, 'w')
     for frame in frames:
-        print (FRAME_STRUCTURE,
-                            frame.glob_index,
-                            frame.jpeg_len,
-                            frame.summary,
-                            frame.successors
-                            )
         f.write(struct.pack(FRAME_STRUCTURE,
                             frame.glob_index,
                             frame.jpeg_len,
@@ -32,3 +31,49 @@ def save_frames_text(frames, filename):
         print >> f, (" summary: %s" % list(frame.summary))
         print >> f, (" successors: %s" % frame.successors)
 
+
+class Frame:
+    def __init__(self, packed=None):
+        if packed is not None:
+            data = struct.unpack(FRAME_STRUCTURE, packed)
+            self.glob_index = data[0]
+            self.jpeg_len = data[1]
+            self.summary = data[2]
+            self.array = np.fromstring(data[2], dtype=np.uint8)
+            self.successors = list(data[3:])
+            self.next = self.successors[0]
+
+
+
+def distance_gen(tail, heads):
+    s = tail.array
+    for h in heads:
+        if h is not tail.head:
+            yield((sum((s - h.array) ** 2), h))
+
+
+def link_frames(frames):
+    tails = []
+    heads = []
+    isbreak = True
+    for f in frames:
+        if isbreak:
+            heads.append(f)
+            isbreak = False
+        if f.successors[0] == 0:
+            f.head = heads[-1]
+            tails.append(f)
+            isbreak = True
+
+    for f in heads + tails:
+        if not hasattr(f, 'array'):
+            f.array =  np.fromstring(f.summary, dtype=np.uint8)
+
+    #print frames, heads, tails
+
+    for t in tails:
+        closest = [x[1] for x in heapq.nsmallest(7, distance_gen(t, heads), key=lambda x: x[0])]
+        t.successors = [0] + [x.index for x in closest]
+        print t.index, t.successors
+
+    return frames
