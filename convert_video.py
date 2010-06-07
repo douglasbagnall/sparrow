1de2a6211c7c5b0b8b8369f637f486988d6e4df4
Douglas Bagnall <douglas@paradise.net.nz>
2010-06-07 16:26:28 +1200

    made double frame configurable option

--- convert_video.py
+++ convert_video.py
@@ -3,7 +3,9 @@
 import os, sys, subprocess
 
 SRC_DIR = "/home/douglas/sparrow/content/dv/"
-DEST_DIR = "/home/douglas/sparrow/content/mjpeg_df/"
+DEST_DIR = "/home/douglas/sparrow/content/mjpeg/"
+#for double frames
+DF_DEST_DIR = "/home/douglas/sparrow/content/mjpeg_df/"
 
 WIDTH = 800
 HEIGHT = 600
@@ -12,7 +14,14 @@ CROP_TOP = 24
 PRE_CROP_RIGHT = 28
 PRE_CROP_TOP = 21
 
-def do_one(src, dest):
+def do_one(src, dest, df=False):
+    if df:
+        yadif_mode = 1
+        fps_list = ['-fps', '50', '-ofps', '50']
+    else:
+        yadif_mode = 0
+        fps_list = []
+
     cmd = ["mencoder",
            "-demuxer", "lavf",
            src,
@@ -20,12 +29,10 @@ def do_one(src, dest):
            "-nosound",
            "-ovc", "lavc",
            "-lavcopts", "vcodec=mjpeg",
-           "-vf", ("yadif=1,hqdn3d,scale=%d:%d,crop=%d:%d:%d:%d" %
-                   (WIDTH + CROP_RIGHT, HEIGHT + CROP_TOP,
+           "-vf", ("yadif=%d,hqdn3d,scale=%d:%d,crop=%d:%d:%d:%d" %
+                   (yadif_mode, WIDTH + CROP_RIGHT, HEIGHT + CROP_TOP,
                     WIDTH, HEIGHT, 0, CROP_TOP)),
-           '-fps', '50',
-           '-ofps', '50',
-           ]
+           ] + fps_list
 
     subprocess.check_call(cmd)
 
@@ -34,34 +41,20 @@ if 0:
            DEST_DIR + "sparrow-1-3-4-12-14145-precrop.avi")
     sys.exit()
 
-def go():
+def go(df=False):
+    if df:
+        destdir = DF_DEST_DIR
+    else:
+        destdir = DEST_DIR
     for src in os.listdir(SRC_DIR):
-        dest = DEST_DIR + src + '.avi'
+        dest = destdir + src + '.avi'
         src = SRC_DIR + src
-        do_one(src, dest)
+        do_one(src, dest, df)
 
 
 go()
 
-#DEST=sparrow-test.avi
 
-#SCALE=scale=1024:768
 
-#REFERENCE_FILTERS=yadif,hqdn3d
-#FILTERS=yadif,hqdn3d,$SCALE
-
-#FILTERS="yadif=1:1,mcdeint=2:1:10,$SCALE"
-
-#FILTERS=yadif,hqdn3d,eq2=1.2:1.0:0.0:1.0:1.0:1.0:0.8:1.0,$SCALE
-#pp=tn/al:f
-#hqdn3d=4:3:6 [:6*3/4]
-#luma_spatial:chroma_spatial:luma_tmp:chroma_tmp
-#ow=8:1.0:1.0
-#
-#eq2[=gamma:contrast:brightness:saturation:rg:gg:bg:weight]
-#2xsai
-
-#FPS=' -fps 25 -ofps 50'
-#mencoder $SRC -o $(basename $SRC .dv).avi -nosound -ovc lavc -lavcopts vcodec=mjpeg -vf $FILTERS $FPS
-
+#hqdn3d=4:3:6 [:6*3/4  == 4]
 
