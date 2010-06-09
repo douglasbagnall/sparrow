5da47fc91224125542e8eec4dd908663c56a932f
Douglas Bagnall <douglas@paradise.net.nz>
2010-06-10 00:02:04 +1200

    use correct paths for loading jpegs

--- load_images.c
+++ load_images.c
@@ -25,8 +25,8 @@
 
 #include <sys/mman.h>
 
-static const char *JPEG_BLOB_NAME = "/home/douglas/sparrow/jpeg.blob";
-static const char *JPEG_INDEX_NAME = "/home/douglas/sparrow/jpeg.index";
+static const char *JPEG_BLOB_NAME = "/home/douglas/sparrow/content/jpeg.blob";
+static const char *JPEG_INDEX_NAME = "/home/douglas/sparrow/content/jpeg.index";
 
 INVISIBLE sparrow_shared_t *
 sparrow_get_shared(){
@@ -43,7 +43,7 @@ load_images(gpointer p){
   lseek(fd, 0, SEEK_SET);
   GST_DEBUG("about to mmap");
   void *mem = mmap(NULL, length, PROT_READ, MAP_PRIVATE | MAP_POPULATE, fd, 0);
-  GST_DEBUG("mmap returned %p", p);
+  GST_DEBUG("mmap returned %p", mem);
   madvise(mem, length, POSIX_MADV_WILLNEED);
   GST_DEBUG("done madvise. sparrow->shared is %p", sparrow->shared);
   sparrow->shared->jpeg_blob = mem;
@@ -67,8 +67,8 @@ load_index(gpointer p){
   off_t length = lseek(fd, 0, SEEK_END);
   lseek(fd, 0, SEEK_SET);
   void *mem = mmap(NULL, length, PROT_READ, MAP_PRIVATE | MAP_POPULATE, fd, 0);
+  GST_DEBUG("mmap returned %p", mem);
   madvise(mem, length, POSIX_MADV_WILLNEED);
-
   sparrow->shared->index = mem;
   sparrow->shared->image_count = length / sizeof(sparrow_frame_t);
   GST_DEBUG("found %d frame info structures of size %d\n", sparrow->shared->image_count,
