d4fec4cb4373442d96351ed611a43f7817a1e5b5
Douglas Bagnall <douglas@paradise.net.nz>
2010-06-18 14:35:22 +1200

    make start.sh work, and set up camera

--- start.sh
+++ start.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 #0 get in place
 
+sleep 20
+
 cd /home/douglas/sparrow
-[[ "$DISPLAY" ]] || $export DISPLAY=:0
+[[ "$DISPLAY" ]] || export DISPLAY=:0
 
 
 #1 initialisation
 #1.1 tune camera?
 
+v4l2ctrl -l /home/douglas/sparrow.conf
+
+#xrandr
 
 #1.2 start up reverse ssh tunnel
 
 #2 go
-
-./gtk-app -f --screens=2 -fps=20 --serial-calibration
-
+while true; do
+    echo hello
+    ./gtk-app -f --screens=2 --fps=20 -c
+done
